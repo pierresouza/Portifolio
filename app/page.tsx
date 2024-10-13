96239ffb368a628be62db935ae83ed09cae6c9d6
pierresouza <pierre.s3@hotmail.com>
2024-10-13 11:57:56 -0300

    feat: organize code and create new area

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,60 @@
+import { TfiLocationPin } from "react-icons/tfi";
 import About from "../components/about";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
       <Header />
-      <div className="flex items-center justify-center gap-20 px-4">
+      <div className="flex flex-col items-center justify-center gap-20">
         <div className="flex flex-col gap-8">
-          <p className="max-w-screen-sm text-5xl">
-            Olá meu nome é pierre Souza e construo páginas web
-          </p>
-          <p className="max-w-screen-sm text-lg">
-            Eu sou formado em Análise e desenvolvimento de sistemas e trabalho
-            como desenvolvedor front-end desde 2022.
-          </p>
+          <div className="flex gap-12">
+            <span className="flex flex-col gap-8">
+              <p className="max-w-screen-sm text-5xl">
+                Olá meu nome é pierre Souza e construo páginas web
+              </p>
+              <p className="text-md max-w-screen-sm text-justify">
+                Como desenvolvedor Front-End com 2 anos de experiência, meu
+                objetivo é criar interfaces que proporcionem uma experiência de
+                usuário incrível e funcional, utilizando as melhores práticas e
+                ferramentas do mercado. Atualmente, estou envolvido em um
+                projeto voluntário que me permite aprimorar minhas habilidades
+                em desenvolvimento Front-End, incorporando tecnologias e
+                frameworks sofisticados e profissionais, como Axios, Tailwind
+                CSS, React e TypeScript.
+              </p>
+            </span>
+            <img
+              src="https://github.com/pierresouza.png"
+              alt=""
+              className="w-96 rounded-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="flex items-center gap-4">
+              <TfiLocationPin />
+              São Paulo, Brasil
+            </span>
+            <span className="flex items-center gap-4">
+              <span className="h-[10px] w-[10px] rounded-full bg-green-400">
+                &nbsp;
+              </span>
+              Disponível para novas oportunidades
+            </span>
+          </div>
+          <div className="flex gap-6">
+            <FiGithub size={24} />
+            <FaTwitterSquare size={24} />
+            <FaLinkedin size={24} />
+          </div>
         </div>
-        <img
-          src="https://github.com/pierresouza.png"
-          alt=""
-          className="w-96 rounded-full"
-        />
+        <About />
+        <Footer />
       </div>
-      <About />
-      <Footer />
     </div>
   );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <footer className="flex flex-col items-center p-4">
-      <p>Desenvolvido por Pierre Souza</p>
-      <p>2024</p>
+      <p>
+        <span>&copy;</span> Desenvolvido com ❤️ por Pierre Souza
+      </p>
     </footer>
   );
 }
