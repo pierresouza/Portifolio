8f15f08f0b3c28127a8861d2e7416764cda60e1a
pierresouza <pierre.s3@hotmail.com>
2024-11-04 08:16:28 -0300

    chore: reorganize items in about area

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,15 +6,13 @@ export default function About() {
     <div className="flex w-full items-center justify-center gap-4">
       <main className="flex h-full w-4/5 flex-col gap-10">
         <p className="text-lg">Tenho conhecimento sobre:</p>
-        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
           {knowledge.map((tech, index) => (
             <div
-              className="flex transform cursor-pointer flex-col-reverse items-center gap-4 rounded-lg border border-black p-4 transition-all duration-300 ease-in-out hover:scale-105 dark:border-white"
+              className="flex h-32 max-w-60 transform cursor-pointer flex-col-reverse items-center justify-center gap-4 rounded-lg border border-black transition-all duration-300 ease-in-out hover:scale-105 dark:border-white"
               key={index}
             >
-              <h2 className="text-nowrap text-sm sm:text-lg md:text-xl lg:text-xl">
-                {tech.title}
-              </h2>
+              <h2 className="text-nowrap text-sm sm:text-lg">{tech.title}</h2>
               {tech.aboutTechs.map((Icon, index) => (
                 <Icon key={index} className="text-5xl" />
               ))}
@@ -22,15 +20,13 @@ export default function About() {
           ))}
         </div>
         <p className="text-lg">Também estou estudando sobre:</p>
-        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
           {studying.map((tech, index) => (
             <div
-              className="flex transform cursor-pointer flex-col-reverse items-center gap-4 rounded-lg border border-black p-4 transition-all duration-300 ease-in-out hover:scale-105 dark:border-white"
+              className="flex h-32 max-w-60 transform cursor-pointer flex-col-reverse items-center justify-center gap-4 rounded-lg border border-black transition-all duration-300 ease-in-out hover:scale-105 dark:border-white"
               key={index}
             >
-              <h2 className="text-nowrap text-sm sm:text-lg md:text-xl lg:text-xl">
-                {tech.title}
-              </h2>
+              <h2 className="text-nowrap text-sm sm:text-lg">{tech.title}</h2>
               {tech.aboutTechs.map((Icon, index) => (
                 <Icon key={index} className="text-5xl" />
               ))}
