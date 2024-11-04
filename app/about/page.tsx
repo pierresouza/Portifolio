fcf6511af6187cf92da43ff332148b043e3b0847
pierresouza <pierre.s3@hotmail.com>
2024-11-04 08:11:19 -0300

    chore: fix quantity items in bigger screens

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <div className="flex w-full items-center justify-center gap-4">
       <main className="flex h-full w-4/5 flex-col gap-10">
         <p className="text-lg">Tenho conhecimento sobre:</p>
-        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {knowledge.map((tech, index) => (
             <div
               className="flex transform cursor-pointer flex-col-reverse items-center gap-4 rounded-lg border border-black p-4 transition-all duration-300 ease-in-out hover:scale-105 dark:border-white"
@@ -22,7 +22,7 @@ export default function About() {
           ))}
         </div>
         <p className="text-lg">Também estou estudando sobre:</p>
-        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+        <div className="grid grid-cols-2 gap-9 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {studying.map((tech, index) => (
             <div
               className="flex transform cursor-pointer flex-col-reverse items-center gap-4 rounded-lg border border-black p-4 transition-all duration-300 ease-in-out hover:scale-105 dark:border-white"
